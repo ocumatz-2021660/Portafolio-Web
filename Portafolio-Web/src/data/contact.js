668a0479bc7d681136cf4c0e51ec9a900e16042e
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-07-25 22:05:26 -0600

    feat: agregando nuevos contactos

--- Portafolio-Web/src/data/contact.js
+++ Portafolio-Web/src/data/contact.js
@@ -1,6 +1,7 @@
 const contact = {
   email: "cumatzsebastian123@gmail.com",
   linkedin: null,
+  instagram: "https://www.instagram.com/cebassx?igsh=ejVhenhpc3d1dGUy",
   github: "https://github.com/ocumatz-2021660",
 }
 
